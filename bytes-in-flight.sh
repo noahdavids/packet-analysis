99d14aa2ca3b9ba70d74ddd1661dadb884783743
Noah Davids <noah@noahdavids.org>
2017-02-18 21:32:38 -0700

    swapped TSHARK-FILTER and OUTPUT-FILE is the usage message and  added 'not tcp.flags.syn == 1' to filter out the SYN segment because tshark does not include tcp.len in the SYN segment so that segment is short a field which screws everything up. Also changed display to show after the sender sends data so if there are multiple sends without an ACK you can see the bytes in flight go up. Added the available window calculation which is the last window minus the bytes in flight

--- bytes-in-flight.sh
+++ bytes-in-flight.sh
@@ -1,22 +1,32 @@
 #!/bin/bash
 # bytes-in-flight.sh begins on the previous line
 #
-# This macro finds the bytes in flight after an ACK is sent. Basically how
-# many bytes does the ACK leave outstanding. This is slightly different
-# from what Wireshark does. Wireshark shows the bytes in flight from when
-# a packet is sent. For example if there are no outstanding bytes and the
-# sender sends 32,000 bytes (we can assume TCP offloading) Wireshark will
-# shows 32,000 bytes in flight. If a series of ACKs acks all bytes and then
-# then sender sends another 32,000 bytes Wireshark will again show 32,000
-# bytes in flight. At no point will it show 0 bytes in flight. I think
-# this is misleading since you cannot see that the receiver is sending
-# multiple ACKs and the bytes in flight is going down to zero and then the
-# sender sends more data. 
+# This macro finds the bytes in flight after each segment. Basically how
+# many bytes are unACKed. This is slightly different from what Wireshark
+# does. Wireshark shows the bytes in flight from when a packet is sent.
+# For example if there are no outstanding bytes and the sender sends
+# 32,000 bytes (we can assume TCP offloading) Wireshark will show 32,000
+# bytes in flight. If a series of ACKs acks all bytes and then the sender
+# sends another 32,000 bytes Wireshark will again show 32,000 bytes in flight.
+# At no point will it show 0 bytes in flight. I think this is misleading
+# since you cannot see that the receiver is sending multiple ACKs and the
+# bytes in flight is going down to zero and then the sender sends more data.
+# It also calculates the avialble window by subtracting the bytes in flight 
+# from the last window receivied.
 #
 # The data probably makes more sense when the trace is captured on the sender
 #
 # Version 1.0 February 4, 2017
-BYTESINFLIGHTVERSION="1.0_2017-02-04"
+# Version 1.1 February 16, 2017
+#  swapped TSHARK-FILTER and OUTPUT-FILE is the usage message and  added
+#  "not tcp.flags.syn == 1" to filter out the SYN segment because tshark
+#  does not include tcp.len in the SYN segment so that segment is short a
+#  field which screws everything up. Also changed display to show after
+#  the sender sends data so if there are multiple sends without an ACK you
+#  can see the bytes in flight go up. Added the available window calculation
+# which is the last window minus the bytes in flight
+#
+BYTESINFLIGHTVERSION="1.1_2017-02-16"
 #
 # This software is provided on an "AS IS" basis, WITHOUT ANY WARRANTY OR ANY
 # SUPPORT OF ANY KIND. The AUTHOR SPECIFICALLY DISCLAIMS ANY IMPLIED WARRANTIES
@@ -35,9 +45,9 @@ if [ $# -ne 7 ]
         echo "      SND-PORT is the TCP Port number sending data"
         echo "      DST-IP is the IP address of the host receiving data"
         echo "      DST-PORT is the TCP Port number receiving data"
-        echo "      OUTPUT-FILE is the name of the output file"
         echo "      TSHARK-FILTER is the filter clause including the \
 -Y or -R "
+        echo "      OUTPUT-FILE is the name of the output file"
         exit
 fi
 
@@ -80,14 +90,15 @@ echo bytes-in-flight.sh $FILE $SNDIP $SNDPORT $DSTIP $DSTPORT $FILTER $OUTPUT
 
 # Filter the trace file for connections matching the SND-IP, SND-PORT,
 # DST-IP, DST-PORT and output the source IP address, TCP sequence
-# number, the TCP length, and ACK number, relative time and frame number.
-# The frame number is to make it easy to go back in the trace and find
-# frames that correspond to areas of interest
+# number, the TCP length, and ACK number, relative time window size and
+# frame number. The frame number is to make it easy to go back in the
+# trace and find frames that correspond to areas of interest
  
 tshark -r $FILE -Y "(ip.addr eq $SNDIP and ip.addr eq $DSTIP) and \
-       (tcp.port eq $SNDPORT and tcp.port eq $DSTPORT)" -T fields \
+       (tcp.port eq $SNDPORT and tcp.port eq $DSTPORT) \
+       and not tcp.flags.syn == 1" -T fields \
        -e ip.src  -e tcp.seq -e tcp.len -e tcp.ack -e tcp.time_relative \
-       -e frame.number >/tmp/bytes-in-flight.out
+       -e tcp.window_size -e frame.number >/tmp/bytes-in-flight.out
 
 # For each line output, if the source IP matches the SND-IP print the
 # source IP and the sum of the TCP sequence number and the TCP length.
@@ -96,21 +107,31 @@ tshark -r $FILE -Y "(ip.addr eq $SNDIP and ip.addr eq $DSTIP) and \
 # of columns. You can parse this arrangement of data but it seemed
 # easier to do it this way. If the IP does not match SND-IP print the
 # source IP and the ACK number. In either case also print the relative
-# time and frame number.
+# time and frame number. Also if the IP matches the sender we do not care
+# about the window size so just use an X. 
 
-awk -v sndip=$SNDIP '{if ($1 == sndip) print $1 " " $2+$3 " " $5 " " $6; \
-                                  else print $1 " " $4 " " $5 " " $6}' \
+awk -v sndip=$SNDIP '{if ($1 == sndip) print $1 " " $2+$3 " " $5 " X " $7; \
+                                  else print $1 " " $4 " " $5 " " $6 " " $7}' \
      /tmp/bytes-in-flight.out > /tmp/bytes-in-flight-2.out
 
 # For each line, if the entry matches SNDIP read the fields in the entry
 # and save the sequence number as sendersequence -- if the sequence value
 # is greater than the previous sendersequence value. If the entry is not
-# SNDIP then output the relative time, sendersequence, sequence, the
-# difference between sendersequence and sequence and the frame number.
-
+# SNDIP then save the sequnce number as acksequence. ACK numbers do not
+# go down so no need to test for that. There will be a discontinuity when
+# seqence numbers wrap. Also if it is not the sender then save the window as
+# lastwindow. Then output the relative time, sendersequence, acksequence,
+# the difference between sendersequence and acksequence and the difference
+# between the lastwindow and difference between the sendersequence and the
+# acksequence. This is the bytes left in the window. Finally include the
+# frame number.
+#
 echo "" > $OUTPUT
 sendersequence=0
-cat /tmp/bytes-in-flight-2.out | while read senderip sequence reltime framenumber; do
+acksequence=0
+lastwindow=0
+cat /tmp/bytes-in-flight-2.out | while read senderip sequence reltime \
+                                                      window framenumber; do
     if [ $senderip == "$SNDIP" ]
       then
         if [ $sequence -gt $sendersequence ]
@@ -118,9 +139,14 @@ cat /tmp/bytes-in-flight-2.out | while read senderip sequence reltime framenumbe
              sendersequence=$sequence
         fi
       else
-        echo $reltime " " $sendersequence " - " $sequence " = " \
-             $(($sendersequence - $sequence)) "    " $framenumber >> $OUTPUT
+        acksequence=$sequence
+        lastwindow=$window
     fi
+    echo $reltime " " $sendersequence " - " $acksequence " = BIF: " \
+             $(($sendersequence - $acksequence)) "    " $lastwindow  \
+             " Avail-Window: " \
+             $(($lastwindow - ($sendersequence - $acksequence))) " " \
+             $framenumber >> $OUTPUT
 done
 
 # clean up temporary files
@@ -132,4 +158,6 @@ rm /tmp/bytes-in-flight-2.out
 # gnuplot, with the plot commands below
 
 # echo "set term x11 persist; plot \"$OUTPUT\" using 1:6" | gnuplot
+#
+# bytes-in-flilght.sh ends here
 
