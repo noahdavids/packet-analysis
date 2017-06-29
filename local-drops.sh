056cf6036b7743ebd6dc6ff17d9b3e050be3b469
Noah Davids <noah@noahdavids.org>
2017-06-28 20:28:28 -0700

    added the pattern string to the output and corrected some typos in the comments

--- local-drops.sh
+++ local-drops.sh
@@ -8,19 +8,26 @@
 # ACK for that sequence number.
 #
 # If this macro is run over a trace that was captured on the receiving host 
-# we can see if the original segment was seen amd if so if an ACK was sent. If
+# we can see if the original segment was seen and if so if an ACK was sent. If
 # this macro is run over a trace that was captured in the sending host
 # we can see if an ACK was received before the retransmitted segment was sent
 #
 # The Output file has the format
-#     TCP Seq NNNNNNN
+#     TCP Seq NNNNNNN Pattern: PPPPPP
 #     frame.number tcp.time_relative ip.src ip.dst tcp.seq tcp.ack tcp.nxtseq
 #     frame.number tcp.time_relative ip.src ip.dst tcp.seq tcp.ack tcp.nxtseq
 #     frame.number tcp.time_relative ip.src ip.dst tcp.seq tcp.ack tcp.nxtseq
 #
 # Multiple frames from the sending IP indicate that the frame was received (or
-# send) multiple times. The placement of the ACK from the receiving IP indicates
+# sent) multiple times. The placement of the ACK from the receiving IP indicates
 # when the ACK was sent.
+#
+# The pattern string PPPPPP represents the pattern of segments.
+#    D-A-   would indicate the first segment is data followed by an ACK
+#    D-D-A- would be data, data again and then an ACK
+#    D-A-D- would be data and ack and then data again. We do not see the second ACK
+#           because only the first ACK is recorded.
+# Counting the unique patterns will give you an idea of the segment loss pattern
 #
 # If the input file is large with many TCP streams it would make sense to first
 # create a file containing just the segments of the TCP stream of interest
@@ -37,8 +44,14 @@
 #    Modified to include the ACK packets
 # Version 1.2 Apr 1 2017
 #    Added copyright and GNU GPL statement and disclaimer
+# Version 1.3 May 15 2017
+#    Have the script figure out to use -Y or -R in tshark command. Also sort
+#    unique the retranmitted sequence numbers so we do get get duplicated
+#    output if the sequence number is retransmitted multiple times.
+# Version 1.4 June 28, 2017
+#    Added the Pattern string 
 
-LOCALDROPSVERSION="1.2_2017-04-01"
+LOCALDROPSVERSION="1.4_2017-06-28"
 
 # from https://github.com/noahdavids/packet-analysis.git
 
@@ -53,7 +66,7 @@ LOCALDROPSVERSION="1.2_2017-04-01"
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 
-if [ $# -ne 5 ]
+if [ $# -ne 4 ]
    then echo "Usage:"
         echo "   local-drops.sh FILE SRC-ADDR TCP-PORT TSHARK-FILTER OUTFILE"
         echo "      FILE is the name of the trace file to be analyzed"
@@ -64,19 +77,16 @@ if [ $# -ne 5 ]
         echo "         to the sender or receiver, it is used to make sure that"
         echo "         only 1 connection is analyzed. (It assumes there is no"
         echo "         port reuse"
-        echo "      TSHARK-FILTER is either Y or R depening on the release \
-of Tshark"
         echo "      OUTFILE is the name of the output file"
         echo "Example:"
-        echo "   local-drops.sh trace.pcap 192.168.1.3 45673 Y local-drops.out"
+        echo "   local-drops.sh trace.pcap 192.168.1.3 45673 local-drops.out"
         exit
 fi
 
 FILE=$1
 IPSRC=$2
 PORT=$3
-FILTER=$4
-OUTFILE=$5
+OUTFILE=$4
 
 if [ ! -e $FILE ]
    then echo "Could not find input file $FILE"
@@ -100,25 +110,30 @@ if [[ ! $PORT =~ ^[0-9]+$ ]]
    exit
 fi
 
-if [ $FILTER != "R" -a $FILTER != "Y" ]
-   then echo "Filter string must be either R or Y, $FILTER is not allowed"
-   exit
+# Figure out if we can use "-Y" as the display filter argument or we need 
+# "-R". Basically look at the help output and if we do not find the "-Y"
+# we use "-R"
+
+DASH="-Y"
+if [ $(tshark -help | egrep "\-Y <display filter>" | wc -l) -eq 0 ]
+then DASH="-R"
 fi
+
 # I always echo the command and arguments to STDOUT as a sanity check
 
-echo local-drops.sh $FILE $IPSRC $PORT $FILTER $OUTFILE
+echo local-drops.sh $FILE $IPSRC $PORT $OUTFILE
 
 # Also echo the command, arguments, date and version to the output file
 
-echo local-drops.sh $FILE $IPSRC $PORT $FILTER $OUTFILE > $OUTFILE
+echo local-drops.sh $FILE $IPSRC $PORT $OUTFILE > $OUTFILE
 echo local-drops.sh run on $(date) >> $OUTFILE
 echo local-drops.sh version $LOCALDROPSVERSION >> $OUTFILE
 echo >> $OUTFILE
 
 
-tshark -r $FILE -$FILTER "ip.src == $IPSRC && tcp.port == $PORT && \
+tshark -r $FILE $DASH "ip.src == $IPSRC && tcp.port == $PORT && \
    tcp.analysis.retransmission" -T fields -e tcp.seq \
-   -o tcp.relative_sequence_numbers:FALSE > /tmp/local_drops_retrans
+   -o tcp.relative_sequence_numbers:FALSE | sort -u > /tmp/local_drops_retrans
 
 NUMBERRETRANS=$(wc -l /tmp/local_drops_retrans | awk '{print $1}')
 echo Numer of retransmission $NUMBERRETRANS >> $OUTFILE
@@ -129,26 +144,31 @@ do
 
    tshark -r $FILE -o tcp.relative_sequence_numbers:FALSE \
                    -o tcp.calculate_timestamps:TRUE \
-       -$FILTER "ip.src == $IPSRC && tcp.port == $PORT && tcp.seq <= $x \
+       $DASH "ip.src == $IPSRC && tcp.port == $PORT && tcp.seq <= $x \
        && $x < tcp.nxtseq" -T fields -e frame.number -e tcp.time_relative \
        -e ip.src -e ip.dst -e tcp.seq -e tcp.ack -e tcp.nxtseq \
        > /tmp/local_drops_frames
 
    tshark -r $FILE -o tcp.relative_sequence_numbers:FALSE \
                    -o tcp.calculate_timestamps:TRUE \
-       -$FILTER "ip.dst == $IPSRC && tcp.port == $PORT && tcp.ack > $x" \
+       $DASH "ip.dst == $IPSRC && tcp.port == $PORT && tcp.ack > $x" \
        -T fields -e frame.number -e tcp.time_relative -e ip.src -e ip.dst \
        -e tcp.seq -e tcp.ack -e tcp.nxtseq | head -1 \
        >> /tmp/local_drops_frames
 
-   echo TCP Seq: $x >> $OUTFILE
-   cat /tmp/local_drops_frames | sort -nk1 >> $OUTFILE
+
+   cat /tmp/local_drops_frames | sort -nk1 > /tmp/local_drops_frames-2
+
+   echo TCP Seq: $x Pattern: $(cat /tmp/local_drops_frames-2 | awk -v ipsrc=$IPSRC \
+            '{ if ($3 == ipsrc) print "D"; else print "A"}' | tr "\n" "-") >> $OUTFILE
+   cat /tmp/local_drops_frames-2 >> $OUTFILE
    echo >> $OUTFILE
 
 done
 
 rm /tmp/local_drops_retrans
 rm /tmp/local_drops_frames
+rm /tmp/local_drops_frames-2
 
 # local-drops.sh ends here
 
