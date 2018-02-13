5d581d636b8d08aabe79855ecbdcd5c27d92d0de
Noah Davids <noah@noahdavids.org>
2018-02-13 04:22:04 -0700

    added the server response code to the output table so you can tell the difference between a server response that provides an answer and one that does not. Update the HTML page to reflect changes.

--- dns-time.sh
+++ dns-time.sh
@@ -8,11 +8,19 @@
 # the first response and last query for transaction X.
 #
 # The output is a a table with the following columns.
-#     Server ID Type Name Response-time - Query-time = Delta-time
+#     Server ID Type Name Rcode Response-time - Query-time = Delta-time
 #
 # It also lists unanswered queries in the table
 #     Server ID Type Name Query-time
-
+#
+# Type ==  1 Type A host address
+# Type == 12 domain name PRT query
+# Type == 28 Type AAAA IPv6 host address
+#
+# Rcode == 0 normal, no error response
+# Rcode == 2 Server failure
+# Rcode == 3 No such name
+#
 # Version 1.0 March 15, 2017
 # Version 1.1 April 01, 2017
 #    Added copyright and GNU GPL statement and disclaimer
@@ -26,8 +34,12 @@
 #    screwed things up as well.
 # Version 1.4 October 12, 2017
 #    Added support for IPv6 client and server addresses
+# Version 1.5 February 13, 2018
+#    Added the dns.flags.rcode to the output to distinguish between a 
+#    response that actually provided an answer and one which did not. Also
+#    added some comments for various DNS types and rcodes.
 
-DNSTMEVERSION="1.2_2017-10-12"
+DNSTMEVERSION="1.5_2018-02-11"
 
 # from https://github.com/noahdavids/packet-analysis.git
 
@@ -73,43 +85,43 @@ fi
 # Scan the packet capture file for DNS traffic and write the frame number
 # the epoch time and human readable time, the IP source and destination, the
 # transaction  ID, a flag for query (0) or response (1), the name being
-# queried, and the query type to /tmp/dns-time-1. Also filter out ICMP records
-# This removes ICMP destination unreachable and any other errors. Finally
-# filter the output to make sure it has only printable and white space
-# characters. This is needed because some dns.qry.name strings have
-# non-printing characters that that causes errors in some of the subsequent
-# commands. Also  make sure that there are 13 fields (frame.time will have 5
-# fields, month day, year, time, time-zone). If the DNS record is malformed we
-# can get a line without complete data which will also screw things up later on.
-# Note that I am selecting both IPv4 and IPv6 addresses only 1 type per frame
-# will print so it doesn't hurt anything to "print" both.
+# queried, the query type and the query response code to /tmp/dns-time-1.
+# Also filter out ICMP records. This removes ICMP destination unreachable and
+# any other errors. Finally filter the output to make sure it has only
+# printable and white space characters. This is needed because some
+# dns.qry.name strings have non-printing characters that that causes errors in
+# some of the subsequent commands. Also  make sure that there are 14 fields
+# (frame.time will have 5 fields, month day, year, time, time-zone). If the
+# DNS record is malformed we can get a line without complete data which will
+# also screw things up later on. Note that I am selecting both IPv4 and IPv6
+# addresses only 1 type per frame will print so it doesn't hurt anything to
+# "print" both.
 
 tshark -r "$FILE" $DASH "not icmp && (udp.port == 53 || tcp.port == 53)" \
       -T fields -e frame.number -e frame.time_epoch -e frame.time \
       -e ip.src -e ipv6.src -e ip.dst -e ipv6.dst \
       -e dns.id -e dns.flags.response -e dns.qry.name \
-      -e dns.qry.type | tr -dc [:graph:][:space:] | \
-       awk '(NF == 13) {print $0}' > /tmp/dns-time-1
+      -e dns.qry.type -e dns.flags.rcode | tr -dc [:graph:][:space:] | \
+       awk '{ if (NF == 14) print $0; else if (NF == 13) print $0 " -"}' \
+       > /tmp/dns-time-1
 
 # For every line in the dns-time-1 which is a response (column 11 (flags)
 # is 1 extract out the Server IP (column 8), client IP (column 9), transaction
-# ID (column 10), the name being queried (column 12) and the query type
-# (column 13).
+# ID (column 10), the name being queried (column 12), the query type (column
+# 13) and the response code (column 14).
 
-awk '($11 == 1) {print $8 " " $9 " " $10 " " $12 " " $13}' /tmp/dns-time-1 | \
-    sort -u | \
-   while read server client id name type
+awk '($11 == 1) {print $8 " " $9 " " $10 " " $12 " " $13 " " $14}' \
+    /tmp/dns-time-1 | sort -u | while read server client id name type rcode
    do
 
-# Echo the server, id, type and name values and then (inside the parens)
-# extract from dns-time-1 the queries by matching on the lines
-# matching the Client, Server, ID, Query Flag (0), name and type and 
-# writing the epoch time (column 2) and the human readable time (column 6)
-# (but not the month, day or year) lines to dns-time-2, keep only the first
-# line then do the same for the response keeping only the first line -- on
-# the theory that the fist answer will move things forward and subsequent
-# answers will be ignored. Note that for the response the order of the IP
-# addresses are reversed, i.e. server then client.
+# Search for the client, server, id, name, type and response == 0 values to get
+# the request time and then server, client, id, name, type response == 1 values
+# to get the matching response time and write the epoch time (column 2) and
+# the human readable time (column 6) (but not the month, day or year) lines to
+# dns-time-2, keep only the first line then do the same for the response
+# keeping only the first line -- on the theory that the fist answer will move
+# things forward and subsequent answers will be ignored. Note that for the
+# response the order of the IP addresses are reversed, i.e. server then client.
 
        grep $client.*$server.*$id.*0.*$name.*$type /tmp/dns-time-1 | \
        awk '{print $2 " " $6}' | head -1 > /tmp/dns-time-2
@@ -123,7 +135,7 @@ awk '($11 == 1) {print $8 " " $9 " " $10 " " $12 " " $13}' /tmp/dns-time-1 | \
 # (column 1). If there aren't two lines just skip it.
 
        if [ $(cat /tmp/dns-time-2 | wc -l) -eq 2 ] 
-          then echo $server $id $type $name \
+          then echo $server $id $type $name $rcode \
             $(cat /tmp/dns-time-2 | tr "\n" " " | \
                     awk '{print $4 " - " $2 " = " $3-$1}')
        fi
@@ -135,7 +147,7 @@ awk '($11 == 1) {print $8 " " $9 " " $10 " " $12 " " $13}' /tmp/dns-time-1 | \
 # put a nice header on it and write out dns-time-3 and run it through
 # column to make a nice table
 
-(echo Server ID Type Name Response-time - Query-time = Delta-time
+(echo Server ID Type Name Rode Respose-time - Query-time = Delta-time
  cat /tmp/dns-time-3) | column -t
 
 
